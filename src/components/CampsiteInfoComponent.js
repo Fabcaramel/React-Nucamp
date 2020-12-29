78a1ab15be72ca206acd9d0df1c421b852dec776
Francesca Pepe <elletroisceo@gmail.com>
2020-12-29 18:15:06 -0500

    Campsite Info andDirectory Component set up

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+
+
+class CampsiteInfo extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle classname="card-title">{campsite.name}</CardTitle>
+            <CardText className>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => (
+            <div>
+              <p>{comment.text}</p>
+              <p>
+                {comment.author}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return <div></div>;
+  }
+
+  render() {
+    if (this.props.campsite) {
+      return (
+        <div>
+          <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+          <div className="row">
+            {this.renderComments(this.props.campsite.comments)}
+          </div>
+        </div>
+      );
+    }
+    return <div>TestFailed</div>;
+  }
+}
+
+export default CampsiteInfo;
